f902f320057f7f81cffb3db9bba00325d45ec82d
VoTanThanhlaptrinh <votanthanh32004@gmail.com>
2025-03-25 13:24:10 +0700

    Tạo project

---
+++ backend/quanlychitieu/src/main/java/com/android_app/quanlychitieu/QuanlychitieuApplication.java
@@ -0,0 +1,13 @@
+package com.android_app.quanlychitieu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlychitieuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlychitieuApplication.class, args);
+	}
+
+}
