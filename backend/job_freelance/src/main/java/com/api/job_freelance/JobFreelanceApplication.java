606870811c743caabff087959d508759c5712a8f
VoTanThanhlaptrinh <votanthanh32004@gmail.com>
2025-05-12 22:53:18 +0700

    chuẩn bị project để code

---
+++ backend/job_freelance/src/main/java/com/api/job_freelance/JobFreelanceApplication.java
@@ -0,0 +1,13 @@
+package com.api.job_freelance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFreelanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobFreelanceApplication.class, args);
+    }
+
+}
